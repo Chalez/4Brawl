fdc11d3769ca61edc25bbfc6adc433502ade398b
Aidan-Smyth <71399934+Aidan-Smyth@users.noreply.github.com>
2022-09-17 16:13:04 -0400

    Runner added
    Adds the runner, which counts and prints a number of strings given to it. For testing outcomes of a repeated action.

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * A playground to test that rolling and runner work
+ */
+public class Main
+{
+    public static void sampleMethod()
+    {
+        Runner r = new Runner();
+        for (int i = 0; i < 9999; i++){
+            Roll a = new Roll(6);
+            r.addResult(String.valueOf(a.getCurrent()));
+        }
+        //r.addResult("a");
+        //r.addResult("b");
+        //r.addResult("c");
+        //r.addResult("d");
+        r.print();
+    }
+}
---
+++ Runner.java
@@ -0,0 +1,45 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    // The string is an input, the Integer is the number of times it's been added.
+    HashMap<String, AtomicInteger> map;
+    // the number of inputs given to the Runner
+    int total;
+
+    // Constructor
+    public Runner()
+    {
+        map = new HashMap<String, AtomicInteger>();
+        total = 0;
+    }
+
+    // Adds an outcome to the map
+    public void addResult(String s)
+    {
+        total++;
+        if(map.containsKey(s)){
+            map.get(s).incrementAndGet();
+        }
+        else{
+            map.put(s, new AtomicInteger(1));
+        }
+    }
+    
+    // Prints the outcomes, the number of times they happened, and a decimal.
+    public void print(){
+        String format = "%-20s%-20s%s%n";
+        Object[] array = map.keySet().toArray();
+        for(int i = 0; i < array.length; i++){
+            System.out.printf(format, String.valueOf(array[i]), map.get(array[i]).toString(), String.valueOf((double)map.get(array[i]).intValue() / total));
+        }
+    }
+}
